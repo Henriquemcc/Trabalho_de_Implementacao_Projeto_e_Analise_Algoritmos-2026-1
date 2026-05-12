53c37e5636118f5e2724805dd8ff1b77656731bd
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-05-12 15:20:27 -0300

    Adicionando classe

--- main.py
+++ main.py
@@ -46,6 +46,20 @@ class SerieTemporal:
         nome = os.path.basename(caminho)
         return SerieTemporal(df, nome)
 
+class MatrizAlinhamento:
+    """
+    Estrutura de dados para armazenamento de uma matriz de alinhamento.
+    """
+    def __init__(self, serie_1: SerieTemporal, serie_2: SerieTemporal):
+        """
+        Constrói uma nova instância da MatrizAlinhamento.
+        :param serie_1: Primeira série temporal (eixo x).
+        :param serie_2: Segunda série temporal (eixo y).
+        """
+        self.serie_1 = serie_1
+        self.serie_2 = serie_2
+
+
 class FramePrincipal(tkinter.Frame):
     """
     Frame principal da interface gráfica.
