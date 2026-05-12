be49bf06dcfbcf77a56b793d84a8dc13d58f05d2
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-05-12 16:10:02 -0300

    Criando janela para selecionar as séries temporais

---
+++ app/view/JanelaSelecionarSerieTemporal.py
@@ -0,0 +1,37 @@
+import tkinter
+
+from pandas.core.reshape import tile
+
+
+class JanelaSelecionarSerieTemporal(tkinter.Tk):
+    def __init__(self, titulo, mensagem, series_temporais: list, confirmar):
+        """
+        Janela utilizada para selecionar uma série temporal.
+        """
+        tkinter.Tk.__init__(self)
+        self.series_temporais = series_temporais
+        self.titulo = titulo
+        self.mensagem = mensagem
+        self.confirmar = confirmar
+        self.title(titulo)
+        self.geometry("300x200")
+
+        # Criando frame
+        self.criar_frame()
+
+    def criar_frame(self):
+        """
+        Cria o frame da série temporal.
+        :return:
+        """
+        variavel_serie_temporal = tkinter.StringVar(value="Série Temporal")
+        tkinter.Label(self, text=self.mensagem, font=("Calibre", 12))
+
+        # Criando um radiobutton para cada série temporal
+        for serie_temporal in self.series_temporais:
+            tkinter.Radiobutton(self, text=serie_temporal.nome, variable=variavel_serie_temporal, value=serie_temporal.nome)
+
+        # Criando um botão de confirmar
+        btn_confirmar = tkinter.Button(self, text="Confirmar", command=self.confirmar)
+
+
