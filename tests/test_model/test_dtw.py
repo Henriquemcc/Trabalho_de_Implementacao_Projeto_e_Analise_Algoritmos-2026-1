25ad614b1a63a93e8b039f419c8d04ccdc3df9ba
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-05-15 13:04:26 -0300

    Adicionando caso de teste

--- tests/test_model/test_dtw.py
+++ tests/test_model/test_dtw.py
@@ -2,6 +2,7 @@ import unittest
 from app.model.SerieTemporal import SerieTemporal
 from app.model.DynamicTimeWarping import DynamicTimeWarping
 from app.model.DynamicTimeWarping import Distancia
+import dtaidistance
 
 
 class TestDTW(unittest.TestCase):
@@ -21,3 +22,11 @@ class TestDTW(unittest.TestCase):
         dtw = DynamicTimeWarping(10, Distancia.EUCLIDIANA)
         resultado = dtw.processar(serie1, serie2)
         self.assertNotEqual(resultado, 0)
+
+    def test_comparar_implementacao_dtw_com_biblioteca(self):
+        serie1 = SerieTemporal([1, 2, 3, 4, 5], "Série Temporal 1")
+        serie2 = SerieTemporal([12, 14, 16, 18], "Série Temporal 2")
+        dtw = DynamicTimeWarping(10, Distancia.EUCLIDIANA)
+        resultado = dtw.processar(serie1, serie2)
+        resultado_biblioteca = dtaidistance.dtw.distance(serie1.dados, serie2.dados, window=10, inner_dist='squared euclidean')
+        self.assertEqual(resultado, resultado_biblioteca)
