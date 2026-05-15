a85e74488dc726c1fae754826300b0a32093c8da
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-05-15 12:34:00 -0300

    Adicionando teste de unidade

---
+++ tests/test_model/test_dtw.py
@@ -0,0 +1,16 @@
+import unittest
+from app.model.SerieTemporal import SerieTemporal
+from app.model.DynamicTimeWarping import DynamicTimeWarping
+from model.DynamicTimeWarping import Distancia
+
+
+class TestDTW(unittest.TestCase):
+    """
+    Realiza testes de unidades na implementação do algoritmo Dynamic time warping.
+    """
+    def test_duas_series_temporais_iguais(self):
+        serie1 = SerieTemporal([1, 2, 3, 4, 5], "Série Temporal 1")
+        serie2 = SerieTemporal([1, 2, 3, 4, 5], "Série Temporal 2")
+        dtw = DynamicTimeWarping(1, Distancia.EUCLIDIANA)
+        resultado = dtw.processar(serie1, serie2)
+        self.assertEqual(resultado, 0)
