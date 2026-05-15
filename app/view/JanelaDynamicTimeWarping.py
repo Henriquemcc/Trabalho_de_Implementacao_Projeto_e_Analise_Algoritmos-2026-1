8c9bc2727d92387f6b54f83f1d9d1f32375d97e1
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-05-15 13:56:24 -0300

    Criando janela para executar o DTW

---
+++ app/view/JanelaDynamicTimeWarping.py
@@ -0,0 +1,61 @@
+import tkinter
+
+from model.DynamicTimeWarping import Distancia
+from model.SerieTemporal import SerieTemporal
+
+
+class JanelaDynamicTimeWarping(tkinter.Toplevel):
+    """
+    Janela utilizada para realizar execução do algoritmo Dynamic Time Warping
+    """
+    def __init__(self, serie_temporal1: SerieTemporal, serie_temporal2: SerieTemporal):
+        """
+        Constrói uma nova instância da JanelaDynamicTimeWarping.
+        :param serie_temporal1: Primeira série temporal.
+        :param serie_temporal2: Segunda série temporal.
+        """
+        tkinter.Toplevel.__init__(self)
+        self.title("Dynamic Time Warping")
+        self.serie_temporal1 = serie_temporal1
+        self.serie_temporal2 = serie_temporal2
+
+        # Configurando o tamanho da janela
+        screen_width = 800
+        screen_height = 600
+        self.geometry(f"{screen_width}x{screen_height}+0+0")
+
+        # Criando frame
+        self.resposta_label = None
+        self.criar_frame()
+
+    def criar_frame(self):
+        """
+        Cria o frame do Dynamic Time Warping
+        :return:
+        """
+        self.frame = tkinter.Frame(self, bg="white")
+
+        # Título do Frame
+        tkinter.Label(self.frame, text="Algoritmo Dynamic Time Warping", font=("Calibre", 20))
+
+        # Criando radiobuttons para escolher a distância utilizada
+        variavel_distancia = tkinter.StringVar()
+        tkinter.Radiobutton(self.frame, text="Distância Euclidiana", variable=variavel_distancia, value=Distancia.EUCLIDIANA).pack()
+        tkinter.Radiobutton(self.frame, text="Distância de Manhattan", variable=variavel_distancia, value=Distancia.MANHATTAN).pack()
+
+        # Criando entrada de texto para escolher a janela de busca
+        tkinter.Label(self.frame, text="Janela de Busca:", font=("Calibre", 12)).pack()
+        variavel_janela_de_busca = tkinter.StringVar()
+        tkinter.Entry(self.frame, textvariable=variavel_janela_de_busca).pack()
+
+        # Criando botão para executar o Dynamic Time Warping
+        tkinter.Button(self.frame, text="Executar", command=lambda: self.executar)
+
+        # Criando label para mostrar a resposta do Dynamic Time Warping
+        self.resposta_label = tkinter.Label(self.frame, text="Resposta:", font=("Calibre", 12))
+        self.resposta_label.pack()
+
+        self.frame.pack(fill=tkinter.BOTH, expand=True)
+
+    def executar(self):
+        pass
