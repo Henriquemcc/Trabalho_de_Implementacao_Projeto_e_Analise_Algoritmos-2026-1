a9d830e54e11c82a6d01820b0124a0418be52861
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-05-15 14:27:17 -0300

    Corrigindo erro de execução do algoritmo DTW

--- app/view/JanelaDynamicTimeWarping.py
+++ app/view/JanelaDynamicTimeWarping.py
@@ -18,6 +18,8 @@ class JanelaDynamicTimeWarping(tkinter.Toplevel):
         self.title("Dynamic Time Warping")
         self.serie_temporal1 = serie_temporal1
         self.serie_temporal2 = serie_temporal2
+        self.mapa_distancias = {d.value: d for d in Distancia}
+        self.variavel_distancia = tkinter.StringVar(value=Distancia.EUCLIDIANA.value)
 
         # Configurando o tamanho da janela
         screen_width = 800
@@ -39,9 +41,8 @@ class JanelaDynamicTimeWarping(tkinter.Toplevel):
         tkinter.Label(self.frame, text="Algoritmo Dynamic Time Warping", font=("Calibre", 20))
 
         # Criando radiobuttons para escolher a distância utilizada
-        self.variavel_distancia = tkinter.StringVar()
-        tkinter.Radiobutton(self.frame, text="Distância Euclidiana", variable=self.variavel_distancia, value=Distancia.EUCLIDIANA).pack()
-        tkinter.Radiobutton(self.frame, text="Distância de Manhattan", variable=self.variavel_distancia, value=Distancia.MANHATTAN).pack()
+        tkinter.Radiobutton(self.frame, text="Distância Euclidiana", variable=self.variavel_distancia, value=Distancia.EUCLIDIANA.value).pack()
+        tkinter.Radiobutton(self.frame, text="Distância de Manhattan", variable=self.variavel_distancia, value=Distancia.MANHATTAN.value).pack()
 
         # Criando entrada de texto para escolher a janela de busca
         tkinter.Label(self.frame, text="Janela de Busca:", font=("Calibre", 12)).pack()
@@ -63,7 +64,11 @@ class JanelaDynamicTimeWarping(tkinter.Toplevel):
         :return:
         """
         janela_de_busca = int(self.variavel_janela_de_busca.get())
-        distancia = Distancia(self.variavel_distancia.get())
-        dtw = DynamicTimeWarping(janela_de_busca, distancia)
+        nome_distancia = self.variavel_distancia.get()
+        distancia_enum = self.mapa_distancias.get(nome_distancia)
+        if distancia_enum is None:
+            raise ValueError(f"Distância '{nome_distancia}' não mapeada.")
+        janela_de_busca = int(self.variavel_janela_de_busca.get())
+        dtw = DynamicTimeWarping(janela_de_busca, distancia_enum)
         resultado = dtw.processar(self.serie_temporal1, self.serie_temporal2)
         self.resposta_label.config(text=f"Resposta: {resultado}")
