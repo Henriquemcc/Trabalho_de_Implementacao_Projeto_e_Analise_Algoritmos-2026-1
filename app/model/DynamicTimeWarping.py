a08862abcb9f8f82fdf8f15be71dab650671644e
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-05-23 17:36:18 -0300

    Adicionando método gerar_matriz_dtw

--- app/model/DynamicTimeWarping.py
+++ app/model/DynamicTimeWarping.py
@@ -1,6 +1,7 @@
 from model.SerieTemporal import SerieTemporal
 import math
 from enum import Enum
+import numpy
 
 class Distancia(Enum):
     EUCLIDIANA = "euclidiana"
@@ -79,3 +80,48 @@ class DynamicTimeWarping:
                                         dtw[i-1, j-1]) # match
 
         return dtw[n, m]
+
+    def gerar_matriz_dtw(self, serie1: SerieTemporal, serie2: SerieTemporal, janela_de_busca: int) -> numpy.ndarray:
+        """
+        Cria uma matriz DTW a partir de duas séries temporais
+        :param serie1: Primeira série temporal.
+        :param serie2: Segunda série temporal.
+        :param janela_de_busca: Tamanho da janela.
+        :return: Matriz DTW.
+        """
+        s = serie1.dados
+        t = serie2.dados
+        n = len(s)
+        m = len(t)
+
+        # Inicializando a matriz dtw com as dimensões (n+1) por (m+1)
+        matriz_dtw = numpy.full((n+1, m+1), numpy.inf)
+
+        # Definindo a condição de contorno inicial
+        matriz_dtw[0, 0] = 0
+
+        # Adaptando a janela de busca
+        w = max(self.janela_de_busca, abs(n - m))
+
+        # Preenchendo a matriz respeitando a janela de restrição
+        for i in range(1, n + 1):
+
+            # Variando o j de acordo com o limite determinado pela janela w
+            limite_inferior = max(1, i - w)
+            limite_superior = min(m, i + w) + 1
+            for j in range(limite_inferior, limite_superior):
+                # Calculando o custo local
+                custo_local = abs(s[i - 1] - t[j - 1])
+
+                # Obtendo o menor vizinho
+                menor_vizinho = min(matriz_dtw[i-1, j], # Deleção ou Inserção
+                                    matriz_dtw[i, j-1], # Inserção ou Deleção
+                                    matriz_dtw[i-1, j-1]) # Match
+
+                # Definindo o valor da posição atual
+                matriz_dtw[i, j] = custo_local + menor_vizinho
+
+        return matriz_dtw
+
+    def dtw_warping_path(self, serie1: SerieTemporal, serie2: SerieTemporal) -> list:
+        pass
