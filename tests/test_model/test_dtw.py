8a31e8d2a7516f95ce37e0c2d1f05105e17e4853
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-05-31 10:25:20 -0300

    Renomeando casos de teste

--- tests/test_model/test_dtw.py
+++ tests/test_model/test_dtw.py
@@ -11,21 +11,21 @@ class TestDTW(unittest.TestCase):
     """
     Realiza testes de unidades na implementação do algoritmo Dynamic time warping.
     """
-    def test_duas_series_temporais_iguais(self):
+    def test_dtw_distance_duas_series_temporais_iguais(self):
         serie1 = SerieTemporal([1, 2, 3, 4, 5], "Série Temporal 1")
         serie2 = SerieTemporal([1, 2, 3, 4, 5], "Série Temporal 2")
         dtw = DynamicTimeWarping(10, Distancia.EUCLIDIANA)
         resultado = dtw.dtw_distance(serie1, serie2)
         self.assertEqual(resultado, 0)
 
-    def test_duas_series_temporais_diferentes(self):
+    def test_dtw_distance_duas_series_temporais_diferentes(self):
         serie1 = SerieTemporal([1, 2, 3, 4, 5], "Série Temporal 1")
         serie2 = SerieTemporal([12, 14, 16, 18], "Série Temporal 2")
         dtw = DynamicTimeWarping(10, Distancia.EUCLIDIANA)
         resultado = dtw.dtw_distance(serie1, serie2)
         self.assertNotEqual(resultado, 0)
 
-    def test_comparar_implementacao_dtw_com_biblioteca(self):
+    def test_dtw_distance_implementacao_com_biblioteca(self):
         serie1 = SerieTemporal([1, 2, 3, 4, 5], "Série Temporal 1")
         serie2 = SerieTemporal([12, 14, 16, 18], "Série Temporal 2")
         dtw = DynamicTimeWarping(10, Distancia.EUCLIDIANA)
@@ -33,7 +33,7 @@ class TestDTW(unittest.TestCase):
         resultado_biblioteca = dtaidistance.dtw.distance(serie1.dados, serie2.dados, window=10, inner_dist='euclidean')
         self.assertEqual(resultado, resultado_biblioteca)
 
-    def test_compara_implementacao_dtw_com_biblioteca_dataset_umd(self):
+    def test_dtw_distance_implementacao_com_biblioteca_dataset_umd(self):
         test = SerieTemporal.abrir_arquivo_tsv('Dataset/UCRArchive_2018/UMD/UMD_TEST.tsv', 0)
         train = SerieTemporal.abrir_arquivo_tsv('Dataset/UCRArchive_2018/UMD/UMD_TRAIN.tsv', 0)
         dtw = DynamicTimeWarping(10, Distancia.EUCLIDIANA)
@@ -41,7 +41,7 @@ class TestDTW(unittest.TestCase):
         resultado_biblioteca = dtaidistance.dtw.distance(test.dados, train.dados, window=10, inner_dist='euclidean')
         self.assertEqual(resultado, resultado_biblioteca)
 
-    def test_compara_implementacao_dtw_com_biblioteca_dataset_smooth_subspace(self):
+    def test_dtw_distance_implementacao_com_biblioteca_dataset_smooth_subspace(self):
         test = SerieTemporal.abrir_arquivo_tsv('Dataset/UCRArchive_2018/SmoothSubspace/SmoothSubspace_TEST.tsv', 0)
         train = SerieTemporal.abrir_arquivo_tsv('Dataset/UCRArchive_2018/SmoothSubspace/SmoothSubspace_TRAIN.tsv', 0)
         dtw = DynamicTimeWarping(10, Distancia.EUCLIDIANA)
@@ -49,7 +49,7 @@ class TestDTW(unittest.TestCase):
         resultado_biblioteca = dtaidistance.dtw.distance(test.dados, train.dados, window=10, inner_dist='euclidean')
         self.assertEqual(resultado, resultado_biblioteca)
 
-    def test_compara_implementacao_dtw_com_biblioteca_dataset_shake_gesture_wiimote_z(self):
+    def test_dtw_distance_implementacao_com_biblioteca_dataset_shake_gesture_wiimote_z(self):
         test = SerieTemporal.abrir_arquivo_tsv('Dataset/UCRArchive_2018/ShakeGestureWiimoteZ/ShakeGestureWiimoteZ_TEST.tsv', 0)
         train = SerieTemporal.abrir_arquivo_tsv('Dataset/UCRArchive_2018/ShakeGestureWiimoteZ/ShakeGestureWiimoteZ_TRAIN.tsv', 0)
         dtw = DynamicTimeWarping(10, Distancia.EUCLIDIANA)
