67503288f0721c0012876e8c15f0599979750956
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-06-05 14:58:02 -0300

    Desenvolvendo o esqueleto do CDTW

--- app/model/ContinuousDynamicTimeWarping.py
+++ app/model/ContinuousDynamicTimeWarping.py
@@ -3,6 +3,11 @@ import numpy
 from model.SerieTemporal import SerieTemporal
 from model.Distancia import Distancia, distancia_manhattan, distancia_euclidiana, distancia_euclidiana_ao_quadrado
 
+class NoCdtw:
+    pass
+
+class CurvaCdtw:
+    pass
 
 class ContinuousDynamicTimeWarping:
     """
@@ -25,3 +30,17 @@ class ContinuousDynamicTimeWarping:
             self.distancia = distancia_euclidiana_ao_quadrado
         else:
             ValueError(f"Tipo de distância inválido: {distancia}")
+
+    def cdtw_distance(self, serie1: SerieTemporal | list | numpy.ndarray, serie2: SerieTemporal | list | numpy.ndarray, interop= 0.3, num_stainer = 5, r = 100) -> float:
+
+        # Retirando o array dados do objeto SerieTemporal da serie1
+        if hasattr(serie1, 'dados'):
+            c1 = serie1.dados
+        else:
+            c1 = serie1
+
+        # Retirando o array dados do objeto SerieTemporal da serie2
+        if hasattr(serie2, 'dados'):
+            c2 = serie2.dados
+        else:
+            c2 = serie2
