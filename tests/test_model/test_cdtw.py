ceda51604670ff7156e950c56c6eb66cf35888be
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-06-06 07:23:43 -0300

    Adicionando caso de teste

--- tests/test_model/test_cdtw.py
+++ tests/test_model/test_cdtw.py
@@ -10,3 +10,9 @@ class TestCdtw(unittest.TestCase):
         serie2 = SerieTemporal([1, 2, 3, 4, 5], "Série Temporal 2")
         resultado = ContinuousDynamicTimeWarping.cdtw_distance(serie1, serie2)
         self.assertEqual(resultado, 0)
+
+    def test_dtw_distance_duas_series_temporais_diferentes(self):
+        serie1 = SerieTemporal([1, 2, 3, 4], "Série Temporal 1")
+        serie2 = SerieTemporal([12, 14, 16, 18], "Série Temporal 2")
+        resultado = ContinuousDynamicTimeWarping.cdtw_distance(serie1, serie2, interpolacao=0)
+        self.assertNotEqual(resultado, 0)
