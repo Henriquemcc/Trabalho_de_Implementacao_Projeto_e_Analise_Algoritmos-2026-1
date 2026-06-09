ccf1932e52e7dbd540e757cf63fb059e54a65312
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-06-09 16:09:08 -0300

    Adicionando caso de teste

--- tests/test_model/test_lcs.py
+++ tests/test_model/test_lcs.py
@@ -24,3 +24,10 @@ class TestLongestCommonSubsequence(unittest.TestCase):
         lcs_pd = LongestCommonSubsequenceProgramacaoDinamica()
         resultado = lcs_pd.lcs(serie1, serie2)
         self.assertEqual(resultado, 5)
+
+    def test_lcs_pd_duas_series_temporais_completamente_diferentes(self):
+        serie1 = SerieTemporal([1, 2, 3, 4, 5], "Série 1")
+        serie2 = SerieTemporal([6, 7, 8, 9, 10], "Série 2")
+        lcs_pd = LongestCommonSubsequenceProgramacaoDinamica()
+        resultado = lcs_pd.lcs(serie1, serie2)
+        self.assertEqual(resultado, 0)
