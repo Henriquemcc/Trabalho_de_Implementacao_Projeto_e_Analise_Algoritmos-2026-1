e2569c1c05199adb2b0ef0bba84c682a6951a572
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-06-10 18:14:22 -0300

    Corrigindo o tamanho da janela do DerivativeDynamicTimeWarping

--- app/view/JanelaDerivativeDynamicTimeWarping.py
+++ app/view/JanelaDerivativeDynamicTimeWarping.py
@@ -22,7 +22,7 @@ class JanelaDerivativeDynamicTimeWarping(tkinter.Toplevel):
         self.variavel_janela_de_busca = tkinter.StringVar(value='10')
 
         # Configurando o tamanho da janela
-        screen_width = 800
+        screen_width = 1200
         screen_height = 600
         self.geometry(f"{screen_width}x{screen_height}+0+0")
 
