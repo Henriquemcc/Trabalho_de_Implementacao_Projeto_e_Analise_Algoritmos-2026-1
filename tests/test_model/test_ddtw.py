416c86cb7a0cee99542f0a9036a583fa2faf98ec
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-06-10 17:28:04 -0300

    Adicionando caso de teste

---
+++ tests/test_model/test_ddtw.py
@@ -0,0 +1,15 @@
+import unittest
+
+from model.DerivativeDynamicTimeWarping import DerivativeDynamicTimeWarping
+from model.SerieTemporal import SerieTemporal
+
+
+class TestDDTW(unittest.TestCase):
+    """
+    Realiza testes de unidades na implementação do algoritmo Derivative Dynamic Time Warping.
+    """
+    def test_ddtw_distancia_duas_series_temporais_iguais(self):
+        serie1 = SerieTemporal([1, 2, 3, 4, 5], "Série Temporal 1")
+        serie2 = SerieTemporal([1, 2, 3, 4, 5], "Série Temporal 2")
+        resultado, _ = DerivativeDynamicTimeWarping.ddtw(serie1, serie2)
+        self.assertEqual(resultado, 0)
