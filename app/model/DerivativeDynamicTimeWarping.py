3a22e59bd1a22757396fef3d5afb648c99e76786
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-06-10 16:33:16 -0300

    Implementando parte principal do algoritmo DDTW

---
+++ app/model/DerivativeDynamicTimeWarping.py
@@ -0,0 +1,78 @@
+from model.SerieTemporal import SerieTemporal
+import numpy
+import collections
+
+
+class DerivativeDynamicTimeWarping:
+    """
+    Implementação do algoritmo Derivative Dynamic Time Warping
+    """
+    @staticmethod
+    def ddtw(serie1: SerieTemporal | list | numpy.ndarray, serie2: SerieTemporal | list | numpy.ndarray, K: int = 10) -> tuple[dict, list[tuple[int, int]]]:
+        """
+        Realiza o cálculo da distância de duas séries temporais através do algoritmo Derivative Dynamic Time Warping.
+        :param serie1: Primeira série temporal.
+        :param serie2: Segunda série temporal.
+        :param K: Largura da janela de restrição global, também conhecido como Janela de Sakoe-Chiba.
+        :return: Matriz de custo acumulado e matriz de rastreamento.
+        Fonte: https://github.com/z2e2/fastddtw/blob/master/_fastddtw.py
+        """
+        # Retirando o array dados do objeto SerieTemporal da serie1
+        if type(serie1).__name__ == 'SerieTemporal':
+            s1 = serie1.dados
+        else:
+            s1 = serie1
+
+        # Retirando o array dados do objeto SerieTemporal da serie2
+        if type(serie2).__name__ == 'SerieTemporal':
+            s2 = serie2.dados
+        else:
+            s2 = serie2
+
+        # Calculando as derivadas de s1 e s2
+        derivada_s1 = DerivativeDynamicTimeWarping.__estimar_derivadas(s1)
+        derivada_s2 = DerivativeDynamicTimeWarping.__estimar_derivadas(s2)
+
+        # Obtendo o tamanho das derivadas
+        tamanho_derivada_s1 = len(derivada_s1)
+        tamanho_derivada_s2 = len(derivada_s2)
+
+        # Alterando o valor de K
+        if K <= abs(tamanho_derivada_s1 - tamanho_derivada_s2):
+            K = 2 * abs(tamanho_derivada_s1 - tamanho_derivada_s2)
+            print('O valor de K foi alterado para {}'.format(K))
+
+        # Definido o valor da janela
+        janela = DerivativeDynamicTimeWarping.__gerar_janela(tamanho_derivada_s1, tamanho_derivada_s2, K)
+
+        # Criando a matriz de custo acumulado com os valores iguais a infinito
+        matriz_custo_acumulado = collections.defaultdict(lambda: (float('inf'),))
+
+        # Definindo o primeiro valor da matriz
+        matriz_custo_acumulado[0, 0] = (0, 0, 0)
+
+        # Interando pelo tamanho da janela
+        for i, j in janela:
+            derivada_t = abs(derivada_s1[i - 1] - derivada_s2[j - 1])
+            matriz_custo_acumulado[i, j] = min((matriz_custo_acumulado[i - 1, j][0]+derivada_t, i-1, j),
+                                               (matriz_custo_acumulado[i, j - 1][0]+derivada_t, i, j-1),
+                                               (matriz_custo_acumulado[i-1, j-1][0]+derivada_t, i-1, j-1), key=lambda a: a[0]
+                                               )
+
+        # Matriz de rastreamento
+        matriz_rastreamento = []
+
+        # Definindo os novos valores para i e j
+        i =  tamanho_derivada_s1
+        j = tamanho_derivada_s2
+        while not(i == j == 0):
+            matriz_rastreamento.append((i - 1, j - 1))
+
+            # Definindo os novos valores para i e j
+            i = matriz_custo_acumulado[i, j][1]
+            j = matriz_custo_acumulado[i, j][2]
+
+        # Invertendo a ordem dos elementos
+        matriz_rastreamento.reverse()
+
+        return matriz_custo_acumulado[tamanho_derivada_s1, tamanho_derivada_s2][0], matriz_rastreamento
