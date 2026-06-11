6c3a3437ad8a775c2152db48eda4a72bac975f15
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-06-11 12:20:42 -0300

    - Mudando referência ao método do controladorl. - Renomeando menus

--- app/view/JanelaPrincipal.py
+++ app/view/JanelaPrincipal.py
@@ -36,12 +36,12 @@ class JanelaPrincipal(tkinter.Tk):
         # Menu transformar
         menu_transformar = tkinter.Menu(barra_menu, tearoff=0)
         menu_transformar.add_command(
-            label="Gerar matriz de alinhamento",
-            command=lambda: controller.gerar_matriz_alinhamento(),
+            label="Gerar matriz de alinhamento Dynamic Time Warping",
+            command=lambda: controller.gerar_matriz_alinhamento_dtw(),
         )
         menu_transformar.add_command(
-            label="Gerar matriz de mapeamento",
-            command=lambda: controller.gerar_matriz_mapeamento(),
+            label="Gerar matriz de mapeamento Dynamic Time Warping",
+            command=lambda: controller.gerar_matriz_mapeamento_dtw(),
         )
         barra_menu.add_cascade(label="Transformar", menu=menu_transformar)
 
