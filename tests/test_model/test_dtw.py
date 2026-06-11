17df47934bdeadc028b3bedcb480f1d09d9ffee9
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-06-11 12:18:39 -0300

    Renomeando método dtw_warping_path para warping_path

--- tests/test_model/test_dtw.py
+++ tests/test_model/test_dtw.py
@@ -65,7 +65,7 @@ class TestDTW(unittest.TestCase):
         serie1 = SerieTemporal([1, 2, 3, 4, 5], "Série Temporal 1")
         serie2 = SerieTemporal([1, 2, 3, 4, 5], "Série Temporal 2")
         dtw = DynamicTimeWarping(10, Distancia.EUCLIDIANA)
-        resultado = dtw.dtw_warping_path(serie1, serie2)
+        resultado = dtw.warping_path(serie1, serie2)
         resultado_biblioteca = dtaidistance.dtw.warping_path(serie1.dados, serie2.dados, window=10)
         self.assertEqual(resultado, resultado_biblioteca)
 
