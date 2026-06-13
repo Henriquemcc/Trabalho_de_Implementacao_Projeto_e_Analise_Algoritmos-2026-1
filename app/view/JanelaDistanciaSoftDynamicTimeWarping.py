25694521ef57dac744336d6e16c42cf908a5f9fb
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-06-13 14:51:50 -0300

    Corrigindo nome de variável

--- app/view/JanelaDistanciaSoftDynamicTimeWarping.py
+++ app/view/JanelaDistanciaSoftDynamicTimeWarping.py
@@ -21,7 +21,7 @@ class JanelaDistanciaSoftDynamicTimeWarping(tkinter.Toplevel):
         self.variavel_gamma = tkinter.StringVar(value='1.0')
 
         # Configurando o tamanho da janela
-        creen_width = 800
+        screen_width = 800
         screen_height = 600
         self.geometry(f"{screen_width}x{screen_height}+0+0")
 
