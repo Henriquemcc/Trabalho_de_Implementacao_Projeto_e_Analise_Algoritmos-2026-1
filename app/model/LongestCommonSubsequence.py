9e293293b1131c80481ca2b62956eefb49d69233
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-06-15 15:07:48 -0300

    Reimplementando o LongestCommonSubsequence

--- app/model/LongestCommonSubsequence.py
+++ app/model/LongestCommonSubsequence.py
@@ -1,23 +1,27 @@
 import numpy
 
 from model import SerieTemporal
+from model.WarpingPathAlgorithm import WarpingPathAlgorithm
 
 
-class LongestCommonSubsequence:
-
+class LongestCommonSubsequence(WarpingPathAlgorithm):
+    """
+    Implementação do algoritmo Longest Common Subsequence.
+    """
     def __init__(self):
-        # Variável que armazena os cálculos anteriores
-        self.__programacao_dinamica = {}
+        """
+        Constrói uma nova instância de LongestCommonSubsequence.
+        """
+        WarpingPathAlgorithm.__init__(self, nome_algoritmo='Longest Common Subsequence')
 
-    def lcs(self, serie1: SerieTemporal | list | numpy.ndarray, serie2: SerieTemporal | list | numpy.ndarray)-> int:
+    def warping_paths(self, serie1: SerieTemporal | list | numpy.ndarray, serie2: SerieTemporal | list | numpy.ndarray)-> tuple[float | int, numpy.ndarray]:
         """
         Realiza o cálculo do comprimento da LongestCommonSubsequence (Sequência Comum Mais Longa) utilizando sua implementação com programação dinâmica.
         :param serie1: Primeira série temporal
         :param serie2: Segunda série temporal
         :return: Número inteiro contendo o comprimento da LongestCommonSubsequence
+        Fonte: https://www.programiz.com/dsa/longest-common-subsequence
         """
-        # Limpando o dicionário de programação dinâmica
-        self.__programacao_dinamica = {}
 
         # Retirando o array dados do objeto SerieTemporal da serie1
         if type(serie1).__name__ == 'SerieTemporal':
@@ -35,33 +39,47 @@ class LongestCommonSubsequence:
         m = len(s1)
         n = len(s2)
 
-        # Executando versão recursiva
-        return self.__lcs_recursiva(s1, s2, m, n)
+        # Iniciando uma matriz de zeros com dimensões (m + 1) por (n + 1)
+        matriz_lcs = [[0 for x in range(n + 1)] for x in range(m + 1)]
 
-    def __lcs_recursiva(self, s1, s2, m, n) -> int:
-        # Caso base: Se ambas os arrays tiverem sido percorridos, o tamanho da LCS seré 0
-        if m == 0 or n == 0:
-            return 0
+        # Construindo a matriz utilizando Programação Dinâmica
+        for i in range(m + 1):
+            for j in range(n + 1):
+                # Caso base: Se ambos os arrays tiverm sido percorridos, o tamanho da LCS será 0
+                if i == 0 or j == 0:
+                    matriz_lcs[i][j] = 0
 
-        # Se os elementos de ambos arrays baterem
-        if s1[m - 1] == s2[n - 1]:
-            soma = 1
-            parametros = (m - 1, n - 1)
-            if parametros not in self.__programacao_dinamica:
-                self.__programacao_dinamica[parametros] = self.__lcs_recursiva(s1, s2, *parametros)
-            return soma + self.__programacao_dinamica[parametros]
+                # Se os elementos de ambos os arrays baterem
+                elif s1[i - 1] == s2[j - 1]:
+                    # Incrementando 1 da diagonal superior esquerda
+                    matriz_lcs[i][j] = matriz_lcs[i - 1][j - 1] + 1
 
-        # Se os elementos de ambos os arrays não baterem
-        # Calculando o máximo do LCS de um com m e n-1 e outro com m-1 e n.
-        else:
-            parametros1 = (m, n - 1)
-            # Executando recursão somente se ela não tiver sido executada anteriormente
-            if parametros1 not in self.__programacao_dinamica:
-                self.__programacao_dinamica[parametros1] = self.__lcs_recursiva(s1, s2, *parametros1)
+                # Se os elementos de ambos arrays não baterem
+                else:
+                    # Obtendo o maior valor possível entre os seus dois vizinhos
+                    matriz_lcs[i][j] = max(matriz_lcs[i - 1][j], matriz_lcs[i][j - 1])
 
-            parametros2 = (m - 1, n)
-            # Executando recursão somente se ela não tiver sido executada anteriormente
-            if parametros2 not in self.__programacao_dinamica:
-                self.__programacao_dinamica[parametros2] = self.__lcs_recursiva(s1, s2, *parametros2)
+        # O valor contido na última célula da matriz é o tamanho da maior subsequência comum encontrada
+        tamanho_maior_subsequencia = matriz_lcs[m][n]
 
-            return max(self.__programacao_dinamica[parametros1], self.__programacao_dinamica[parametros2])
+        # Criando um array para armazenar a maior subsequência
+        maior_subsequencia = []
+
+        # Realizando o Backtracking da matriz utilizada na programação dinâmica
+        i = m
+        j = n
+        while i > 0 and j > 0:
+            # Se o elemento da primeira série for igual ao elemento da segunda série na posição atual
+            if s1[i - 1] == s2[j - 1]:
+                maior_subsequencia.insert(0, s1[i - 1])
+                i -= 1
+                j -= 1
+
+            # Se o elemento da primeira série for diferente do elemento da segunda série na posição atual, será analisado o maior valor
+            elif matriz_lcs[i - 1][j] > matriz_lcs[i][j - 1]:
+                i -= 1
+            else:
+                j -= 1
+
+        return tamanho_maior_subsequencia, numpy.array(maior_subsequencia)
+
--- tests/test_model/test_lcs.py
+++ tests/test_model/test_lcs.py
@@ -9,12 +9,12 @@ class TestLongestCommonSubsequence(unittest.TestCase):
         serie1 = SerieTemporal([1, 2, 3, 4, 5], "Série 1")
         serie2 = SerieTemporal([1, 2, 3, 4, 5], "Série 2")
         lcs = LongestCommonSubsequence()
-        resultado = lcs.lcs(serie1, serie2)
+        resultado, _ = lcs.warping_paths(serie1, serie2)
         self.assertEqual(resultado, 5)
 
     def test_lcs_pd_duas_series_temporais_completamente_diferentes(self):
         serie1 = SerieTemporal([1, 2, 3, 4, 5], "Série 1")
         serie2 = SerieTemporal([6, 7, 8, 9, 10], "Série 2")
         lcs = LongestCommonSubsequence()
-        resultado = lcs.lcs(serie1, serie2)
+        resultado, _ = lcs.warping_paths(serie1, serie2)
         self.assertEqual(resultado, 0)
