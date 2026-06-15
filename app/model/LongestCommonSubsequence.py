c74106fae66acc7520006388939dc2bc84c9b6b3
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-06-15 13:39:00 -0300

    Removendo excesso de returns

--- app/model/LongestCommonSubsequence.py
+++ app/model/LongestCommonSubsequence.py
@@ -38,27 +38,30 @@ class LongestCommonSubsequence:
     def __lcs_recursiva(self, s1, s2, m, n) -> int:
         # Caso base: Se ambas os arrays tiverem sido percorridos, o tamanho da LCS seré 0
         if m == 0 or n == 0:
-            return 0
+            tamanho = 0
 
-        # Se os elementos de ambos arrays baterem
-        if s1[m - 1] == s2[n - 1]:
-            soma = 1
-            parametros = (m - 1, n - 1)
-            if parametros not in self.__programacao_dinamica:
-                self.__programacao_dinamica[parametros] = self.__lcs_recursiva(s1, s2, *parametros)
-            return soma + self.__programacao_dinamica[parametros]
-
-        # Se os elementos de ambos os arrays não baterem
-        # Calculando o máximo do LCS de um com m e n-1 e outro com m-1 e n.
         else:
-            parametros1 = (m, n - 1)
-            # Executando recursão somente se ela não tiver sido executada anteriormente
-            if parametros1 not in self.__programacao_dinamica:
-                self.__programacao_dinamica[parametros1] = self.__lcs_recursiva(s1, s2, *parametros1)
+            # Se os elementos de ambos arrays baterem
+            if s1[m - 1] == s2[n - 1]:
+                soma = 1
+                parametros = (m - 1, n - 1)
+                if parametros not in self.__programacao_dinamica:
+                    self.__programacao_dinamica[parametros] = self.__lcs_recursiva(s1, s2, *parametros)
+                tamanho = soma + self.__programacao_dinamica[parametros]
 
-            parametros2 = (m - 1, n)
-            # Executando recursão somente se ela não tiver sido executada anteriormente
-            if parametros2 not in self.__programacao_dinamica:
-                self.__programacao_dinamica[parametros2] = self.__lcs_recursiva(s1, s2, *parametros2)
+            # Se os elementos de ambos os arrays não baterem
+            # Calculando o máximo do LCS de um com m e n-1 e outro com m-1 e n.
+            else:
+                parametros1 = (m, n - 1)
+                # Executando recursão somente se ela não tiver sido executada anteriormente
+                if parametros1 not in self.__programacao_dinamica:
+                    self.__programacao_dinamica[parametros1] = self.__lcs_recursiva(s1, s2, *parametros1)
 
-            return max(self.__programacao_dinamica[parametros1], self.__programacao_dinamica[parametros2])
+                parametros2 = (m - 1, n)
+                # Executando recursão somente se ela não tiver sido executada anteriormente
+                if parametros2 not in self.__programacao_dinamica:
+                    self.__programacao_dinamica[parametros2] = self.__lcs_recursiva(s1, s2, *parametros2)
+
+                tamanho = max(self.__programacao_dinamica[parametros1], self.__programacao_dinamica[parametros2])
+
+        return tamanho
